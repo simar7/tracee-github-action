f2ad319084c55a0dcbf400dbe56b619244171f35
Simarpreet Singh <simar@linux.com>
2021-04-29 11:14:27 -0700

    print messages every second up to a minute

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello Tracee!")
+	for i := 0; i < 60; i++ {
+		fmt.Printf("Tick %d\n", i)
+		time.Sleep(time.Second)
+	}
 }
